0def5dcc971be6ad5e70d64982ee26b4265eaa6c
Corean Gonzales <coreangonzales@gmail.com>
2019-03-10 22:27:07 -0500

    Added React App Component

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const title = "Corean's Auth App";
+
+
+class App extends React.Component {
+  render() {
+    return(
+      <div>{title}</div>
+    )
+  }
+}
+
+export default App;
